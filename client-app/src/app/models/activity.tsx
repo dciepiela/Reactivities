1310fae875ccca47c68a25dcaa7cf748aac02e95
dciepiela <d.ciepiela00@gmail.com>
2023-12-16 21:55:51 +0100

    End of section 5

---
+++ client-app/src/app/models/activity.tsx
@@ -0,0 +1,10 @@
+// lub export type Activity = {}
+export interface Activity {
+    id: string
+    title: string
+    date: string
+    description: string
+    category: string
+    city: string
+    venue: string
+}
